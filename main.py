ee2b5d3cb287f0ccedc23e0b66b0f89fe64725a0
kiaagr012 <kianagrawal126@gmail.com>
2024-11-13 20:00:56 -0800

    Add files via upload

---
+++ main.py
@@ -0,0 +1,15 @@
+student = ('Kian',11, 'tall')
+print(student)
+
+for i in student:
+    print(i)
+
+nTupple = ('mouse', [4,5,6,7,8,9], (1,2,3))
+print(nTupple)
+print(nTupple[1])
+
+random = ('mom', 37, 'likes relaxing', 'dad', 40, 'likes sleeping', 'Kian', 11, 'likes doing nothing')
+print(random)
+print(random[0:3])
+print(random[3:6])
+print(random[6:9])
